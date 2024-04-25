6cfc4e0b4c1cbd5de276de5bbe40805dc7d4c67b
sese404 <sebastian.pistor@gmail.com>
2024-04-25 21:22:35 +0200

    true false optimisation possible instead of 0 and 1

--- src/GameOfLife.jl
+++ src/GameOfLife.jl
@@ -16,187 +16,187 @@ using LinearAlgebra
     Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 """
 
-function game_of_life!(fields::Matrix{UInt8},iterations::UInt64)
+function game_of_life!(fields::Matrix{Int8},_iterations::UInt64)
     
     rows = size(fields, 1)
     columns = size(fields, 2)
-    new_fields = zeros(UInt8, rows, columns)
+    new_fields = zeros(Int8, rows, columns)
     cond = 0
 
-    for _ in 1:1:iterations
+    # for _ in 1:1:iterations
 
-        """
-            Handles the 4 corners of the matrix
-        """
-        cond = sum(fields[1:1:2,1:1:2])
-        if fields[1,1] == 1
-            # Center field must be == 1 we check for !=3 instead of !=2
-            if 2 < cond < 5
-                new_fields[1,1] = 1
-            else
-                new_fields[1,1] = 0
-            end
+    """
+        Handles the 4 corners of the matrix
+    """
+    cond = sum(fields[1:1:2,1:1:2])
+    if fields[1,1] == 1
+        # Center field must be == 1 we check for !=3 instead of !=2
+        if 2 < cond < 5
+            new_fields[1,1] = 1
         else
-            if cond == 3
-                new_fields[1,1] = 1
-            end
+            new_fields[1,1] = 0
         end
-        cond = sum(fields[1:1:2,columns-1:1:columns])
-        if fields[1,columns] == 1
-            # Center field must be == 1 we check for !=3 instead of !=2
-            if 2 < cond < 5
-                new_fields[1,columns] = 1
-            else
-                new_fields[1,columns] = 0
-            end
-        else
-            if cond == 3
-                new_fields[1,columns] = 1
-            end
+    else
+        if cond == 3
+            new_fields[1,1] = 1
         end
-        cond = sum(fields[1:1:2,columns-1:1:columns])
-        if fields[rows,1] == 1
-            # Center field must be == 1 we check for !=3 instead of !=2
-            if 2 < cond < 5
-                new_fields[rows,1] = 1
-            else
-                new_fields[rows,1] = 0
-            end
-        else
-            if cond == 3
-                new_fields[rows,1] = 1
-            end
-        end
-        cond = sum(fields[rows-1:1:rows,columns-1:1:columns])
-        if fields[rows,columns] == 1
-            # Center field must be == 1 we check for !=3 instead of !=2
-            if 2 < cond < 5
-                new_fields[rows,columns] = 1
-            else
-                new_fields[rows,columns] = 0
-            end
-        else
-            if cond == 3
-                new_fields[rows,columns] = 1
-            end
-        end
-
-
-        """
-            Handles the first and last row of the matrix except for the corners
-        """
-        for column in 2:1:columns-1
-            
-            cond = sum(fields[1:1:2,column-1:1:column+1])
-            if fields[1,column] == 1
-                # Center field must be == 1 we check for !=3 instead of !=2
-                if 2 < cond < 5
-                    new_fields[1,column] = 1
-                else
-                    new_fields[1,column] = 0
-                end
-            else
-                if cond == 3
-                    new_fields[1,column] = 1
-                end
-            end
-            cond = sum(fields[rows-1:1:rows,column-1:1:column+1])
-            if fields[rows,column] == 1
-                # Center field must be == 1 we check for !=3 instead of !=2
-                if 2 < cond < 5
-                    new_fields[rows,column] = 1
-                else
-                    new_fields[rows,column] = 0
-                end
-            else
-                if cond == 3
-                    new_fields[rows,column] = 1
-                end
-            end
-
-        end
-
-
-        for row in 2:1:rows-1
-
-            """
-                Handles the first and last column of the matrix except for the corners
-            """
-            cond = sum(fields[row-1:1:row+1,1:1:2])
-            if fields[row,1] == 1
-                # Center field must be == 1 we check for !=3 instead of !=2
-                if 2 < cond < 5
-                    new_fields[row,1] = 1
-                else
-                    new_fields[row,1] = 0
-                end
-            else
-                if cond == 3
-                    new_fields[row,1] = 1
-                end
-            end
-            cond = sum(fields[row-1:1:row+1,columns-1:1:columns])
-            if fields[row,columns] == 1
-                # Center field must be == 1 we check for !=3 instead of !=2
-                if 2 < cond < 5
-                    new_fields[row,columns] = 1
-                else
-                    new_fields[row,columns] = 0
-                end
-            else
-                if cond == 3
-                    new_fields[row,columns] = 1
-                end
-            end
-
-
-            """
-                Handles the rest of the matrix
-            """
-            for column in 2:1:columns-1
-                cond = sum(fields[row-1:1:row+1,column-1:1:column+1])
-                if fields[row,column] == 1
-                    # since the center field must be == 1 we check for !=3 instead of !=2
-                    if 2 < cond < 5
-                        new_fields[row,column] = 1
-                    else
-                        new_fields[row,column] = 0
-                    end
-                else
-                    if cond == 3
-                        new_fields[row,column] = 1
-                    end
-                end
-            end
-
-        end
-        fields .= new_fields
-        
     end
+    cond = sum(fields[1:1:2,columns-1:1:columns])
+    if fields[1,columns] == 1
+        # Center field must be == 1 we check for !=3 instead of !=2
+        if 2 < cond < 5
+            new_fields[1,columns] = 1
+        else
+            new_fields[1,columns] = 0
+        end
+    else
+        if cond == 3
+            new_fields[1,columns] = 1
+        end
+    end
+    cond = sum(fields[1:1:2,columns-1:1:columns])
+    if fields[rows,1] == 1
+        # Center field must be == 1 we check for !=3 instead of !=2
+        if 2 < cond < 5
+            new_fields[rows,1] = 1
+        else
+            new_fields[rows,1] = 0
+        end
+    else
+        if cond == 3
+            new_fields[rows,1] = 1
+        end
+    end
+    cond = sum(fields[rows-1:1:rows,columns-1:1:columns])
+    if fields[rows,columns] == 1
+        # Center field must be == 1 we check for !=3 instead of !=2
+        if 2 < cond < 5
+            new_fields[rows,columns] = 1
+        else
+            new_fields[rows,columns] = 0
+        end
+    else
+        if cond == 3
+            new_fields[rows,columns] = 1
+        end
+    end
+
+
+    """
+        Handles the first and last row of the matrix except for the corners
+    """
+    @inbounds for column in 2:1:columns-1
+        
+        cond = sum(fields[1:1:2,column-1:1:column+1])
+        if fields[1,column] == 1
+            # Center field must be == 1 we check for !=3 instead of !=2
+            if 2 < cond < 5
+                new_fields[1,column] = 1
+            else
+                new_fields[1,column] = 0
+            end
+        else
+            if cond == 3
+                new_fields[1,column] = 1
+            end
+        end
+        cond = sum(fields[rows-1:1:rows,column-1:1:column+1])
+        if fields[rows,column] == 1
+            # Center field must be == 1 we check for !=3 instead of !=2
+            if 2 < cond < 5
+                new_fields[rows,column] = 1
+            else
+                new_fields[rows,column] = 0
+            end
+        else
+            if cond == 3
+                new_fields[rows,column] = 1
+            end
+        end
+
+    end
+
+
+    @inbounds for row in 2:1:rows-1
+
+        """
+            Handles the first and last column of the matrix except for the corners
+        """
+        cond = sum(fields[row-1:1:row+1,1:1:2])
+        if fields[row,1] == 1
+            # Center field must be == 1 we check for !=3 instead of !=2
+            if 2 < cond < 5
+                new_fields[row,1] = 1
+            else
+                new_fields[row,1] = 0
+            end
+        else
+            if cond == 3
+                new_fields[row,1] = 1
+            end
+        end
+        cond = sum(fields[row-1:1:row+1,columns-1:1:columns])
+        if fields[row,columns] == 1
+            # Center field must be == 1 we check for !=3 instead of !=2
+            if 2 < cond < 5
+                new_fields[row,columns] = 1
+            else
+                new_fields[row,columns] = 0
+            end
+        else
+            if cond == 3
+                new_fields[row,columns] = 1
+            end
+        end
+
+
+        """
+            Handles the rest of the matrix
+        """
+        @inbounds for column in 2:1:columns-1
+            cond = sum(fields[row-1:1:row+1,column-1:1:column+1])
+            if fields[row,column] == 1
+                # since the center field must be == 1 we check for !=3 instead of !=2
+                if 2 < cond < 5
+                    new_fields[row,column] = 1
+                else
+                    new_fields[row,column] = 0
+                end
+            else
+                if cond == 3
+                    new_fields[row,column] = 1
+                end
+            end
+        end
+
+    end
+    fields .= new_fields
+        
+    # end
 
 end
 
 function play(iterations::UInt64)
-    rows::UInt8 = 10
-    columns::UInt8 = 10
-    fields = zeros(UInt8, rows, columns)
-    fields[6:7,4] .= 1
-    fields[6:7,5] .= 1
-    fields[6:7,6] .= 1
-    fields[6:7,7] .= 1
-    fields[6:7,8] .= 1
-    fields[6:7,9] .= 1
+    rows::UInt8 = 20
+    columns::UInt8 = 20
+    fields = zeros(Int8, rows, columns)
+    fields[9,9:11] .= 1
+    fields[11,9:11] .= 1
+    fields[10,12] = 1
+    fields[10,8] = 1
     for row in 1:rows
         println(fields[row,1:columns])
     end
-    @time game_of_life!(fields,iterations)
-    for row in 1:rows
-        println(fields[row,1:columns])
+    for _ in 1:1:iterations
+        @time game_of_life!(fields,iterations)
+        for row in 1:rows
+            println(fields[row,1:columns])
+        end
     end
 end
 
 end # module GameOfLife
 
 using .GameOfLife: play
-iterations::UInt64 = 1
-play(iterations)
+iterations::UInt64 = 20
+@showtime play(iterations)
